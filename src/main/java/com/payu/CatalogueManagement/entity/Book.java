2838e3bee117039b1c23c51a0ab921e054ac375a
liyabona saki <liyabonasaki@gmail.com>
2025-08-28 10:51:02 +0200

    add opentelemetry for logging

--- src/main/java/com/payu/CatalogueManagement/entity/Book.java
+++ src/main/java/com/payu/CatalogueManagement/entity/Book.java
@@ -2,8 +2,14 @@ package com.payu.CatalogueManagement.entity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Builder // <== this is required for Book.builder() to work
 @Entity
 public class Book {
 
@@ -81,3 +87,35 @@ public class Book {
         this.bookType = bookType;
     }
 }
+
+/*
+package com.payu.CatalogueManagement.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(hidden = true)
+    private Long id;
+
+    private String name;
+    private String isbnNumber;
+    private LocalDate publishDate;
+    private Double price;
+
+    @Enumerated(EnumType.STRING)
+    private BookType bookType;
+}
+ */
+
